52e1120c9f2da329695dfa80215e20a2eabc4a9d
Rajaram Lakshmanan <rajaram.lakshmanan@siemens-healthineers.com>
2025-04-17 14:19:10 +0530

    Base project created

---
+++ Traki/Traki/Traki.Mac/MauiProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace Traki.Mac
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseSharedMauiApp();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Traki/Traki/Traki.iOS/MauiProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace Traki.iOS
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseSharedMauiApp();
+
+            return builder.Build();
+        }
+    }
+}
