3c8abc5548f6bef448b6f3765208ee0a7eb2755b
Rajaram Lakshmanan <rajaram.lakshmanan@siemens-healthineers.com>
2025-04-20 08:01:03 +0530

    Just supressed the warnings

--- Traki/Core/ViewModels/SharedHeaderViewModel.cs
+++ Traki/Core/ViewModels/SharedHeaderViewModel.cs
@@ -10,45 +10,40 @@ namespace Core.ViewModels
 {
     public partial class SharedHeaderViewModel : ObservableObject
     {
-        //#region Filter
+        #region Filter 
+#pragma warning disable 
+        [ObservableProperty] private bool isIntervalFilterSelected;
+        [ObservableProperty] private bool isDateFilterSelected;
+        [ObservableProperty] private bool isMonthFilterSelected;
+        [ObservableProperty] private bool isWeekFilterSelected;
+        [ObservableProperty] private bool isYearFilterSelected;
+        [ObservableProperty] private string? selectedFilterOption;
+#pragma warning restore 
 
-        //[ObservableProperty] private bool isIntervalFilterSelected = false;
-        //[ObservableProperty] private bool isDateFilterSelected = false;
-        //[ObservableProperty] private bool isMonthFilterSelected;
-        //[ObservableProperty] private bool isWeekFilterSelected;
-        //[ObservableProperty] private bool isYearFilterSelected;
-        //public bool IsAnyFilterVisible =>
-        //            IsWeekFilterSelected || IsMonthFilterSelected || IsYearFilterSelected || IsIntervalFilterSelected || IsDateFilterSelected;
+        public bool IsAnyFilterVisible =>
+            IsWeekFilterSelected || IsMonthFilterSelected || IsYearFilterSelected || IsIntervalFilterSelected || IsDateFilterSelected;
 
-        //[ObservableProperty]
-        //private string selectedFilterOption;// String.Empty;
-        //partial void OnSelectedFilterOptionChanged(string value)
-        //{
+        partial void OnSelectedFilterOptionChanged(string? value)
+        {
+            IsDateFilterSelected = value == "Choose Date";
+            IsIntervalFilterSelected = value == "Interval";
+            IsMonthFilterSelected = value == "Month";
+            IsWeekFilterSelected = value == "Week";
+            IsYearFilterSelected = value == "Year";
 
-        //    IsDateFilterSelected = value == "Choose Date";
-        //    IsIntervalFilterSelected = value == "Interval";
-        //    IsMonthFilterSelected = value == "Month";
-        //    IsWeekFilterSelected = value == "Week";
-        //    IsYearFilterSelected = value == "Year";
+            if (value == "Day")
+            {
+                var today = DateTime.Today;
+                var endOfDay = today.AddDays(1).AddTicks(-1);
+                // FilterTransactionsByRange(today, endOfDay);
+            }
+        }
+        #endregion Filter
 
-        //    if (value == "Day")
-        //    {
-        //        var today = DateTime.Today; // midnight (00:00)
-        //        var endOfDay = today.AddDays(1).AddTicks(-1); // 23:59:59.9999999
-        //        //FilterTransactionsByRange(today, endOfDay);
-        //    }
-
-
-        //    //OnPropertyChanged(nameof(IsDateFilterSelected));
-        //    //OnPropertyChanged(nameof(IsIntervalFilterSelected));
-        //    //OnPropertyChanged(nameof(IsAnyFilterVisible));
-        //}
-        //#endregion Filter
-
-        //#region DB
-        //private readonly SQLiteAsyncConnection? _database;
-        //private readonly IAccountService? _accountService;
-        //#endregion DB
+        #region DB
+        private readonly SQLiteAsyncConnection? _database;
+        private readonly IAccountService? _accountService;
+        #endregion DB
 
         //#region Collection
         //[ObservableProperty]
